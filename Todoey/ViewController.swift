c36ee383cd9b92c59eeb213ad72b8db511462222
Justin Huy <trannhathuy1305@gmail.com>
2019-10-19 21:03:50 +0700

    asd

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by JustinHuy on 10/19/19.
+//  Copyright © 2019 JustinHuy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
